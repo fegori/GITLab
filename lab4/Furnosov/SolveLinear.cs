64a71ff4f0158266ef41e5a1de06af12151a39bd
Fegori <fegori@yandex.ru>
2021-12-16 00:53:23 +0300

    Fourth

---
+++ lab4/Furnosov/SolveLinear.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Furnosov
+{
+    class SolveLinear
+    {
+        protected List<float> X;
+        protected List<float> Linear(float a, float b)
+        {
+            if (a == 0)
+            {
+                throw new FurnosovException("Определено, что такое уравнение не существует.");
+            }
+            FurnosovLog.I().log("Определено, что это линейное уравнение.");
+            return X = new List<float>() { -b / a };
+        }
+    }
+}
---
+++ lab4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Furnosov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 3 параметра a, b и с.");
+            //FurnosovLog.I().log("Введите 3 параметра a, b и с.");
+            try
+            {
+                float a = float.Parse(Console.ReadLine());
+                float b = float.Parse(Console.ReadLine());
+                float c = float.Parse(Console.ReadLine());
+                FurnosovLog.I().log($"Введено уравнение: {a}*x^2 + {b}*x + {c} = 0");
+                SolveQuadratic object1 = new SolveQuadratic();
+                List<float> result=object1.solve(a,b,c);
+                FurnosovLog.I().log ("Корни уравнения: "+string.Join("   ", result));
+            }
+            catch (FurnosovException ex1)
+            {
+                FurnosovLog.I().log(ex1.Message);
+            }
+            FurnosovLog.I().write();
+            Console.ReadKey();
+        }
+        
+    }
+}
