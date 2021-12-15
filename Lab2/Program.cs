d57c097ad0ed4a8ae0eb3ebe351751835ed93f30
Fegori <fegori@yandex.ru>
2021-12-16 00:46:59 +0300

    Second

---
+++ Lab2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab2
+{
+    class Program
+    {
+        class A
+        {
+           protected List<double> X;
+           protected List<double> Linear(double a, double b)
+           {
+                if (a == 0)
+                {
+                    throw new Exception("НЕ УРАВНЕНИЕ");
+                }
+                return X =  new List<double>() { -b / a }; 
+           }
+        }
+        class B : A
+        {
+            protected double Discriminant(double a, double b, double c )
+            {
+                return b * b - 4 * c * a;
+            }
+            List<double> Quadratic(double a, double b,double c)
+            {
+                if (a == 0)
+                {
+                    return Linear(b, c);
+                }
+                double discriminant = Discriminant(a, b, c);
+                if (discriminant < 0)
+                {
+                    throw new Exception("Нет решения, т.к. дискриминант равен 0");
+                }
+                if (discriminant == 0)
+                {
+                    return X = new List<double>() { -b / 2 / a }; 
+                }
+                double sqrtDiscriminant = Math.Sqrt(discriminant);
+                return new List<double> {(-b - sqrtDiscriminant) / 2 / a, (-b + sqrtDiscriminant) / 2 / a };
+            }
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
